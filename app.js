8b2052c3c99ae83e1e7fa6831bd00be03dc5296f
zaidShishani <zaidjamo94@Gmail.com>
2021-08-01 16:12:15 +0300

    added app.js css and index

---
+++ app.js
@@ -0,0 +1,17 @@
+let name = prompt("what is your name ??")
+
+alert('Hello ' + name + " to my website :D")
+
+console.log("asked for name: " + name +  ", data type: " + typeof name);
+
+let age = Number(prompt("how old are you ??"))
+
+console.log("asked for age: " + age +  ", data type: " + typeof age);
+
+let favFood = prompt("what is your what is your favorite food ??")
+
+console.log("asked for their favorite food: " + favFood +  ", data type: " + typeof favFood);
+
+let freTime = prompt("what do you do in your free time ??")
+
+console.log("askig about hobbies and interest: " + freTime +  ", data type: " + typeof freTime);
